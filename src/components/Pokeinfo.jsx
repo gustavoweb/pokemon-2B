1a215b9224ecdbff8d8e4af2c07f5063e0d6b705
gustavoweb <contato@gustavoweb.com>
2025-04-04 10:20:24 -0300

    Add files via upload

---
+++ src/components/Pokeinfo.jsx
@@ -0,0 +1,11 @@
+import React, {useState} from 'react';
+import './Pokeinfo.css';
+
+function Pokeinfo(){
+    return(
+        <>
+            <h1>Informações sobre Pokemon</h1>
+        </>
+    );
+};
+export default Pokeinfo;
