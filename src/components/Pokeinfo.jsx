4272f5e1f60782232b3159fdb64397b0122e4a20
gustavoweb <contato@gustavoweb.com>
2025-04-07 07:29:55 -0300

    Add files via upload

--- src/components/Pokeinfo.jsx
+++ src/components/Pokeinfo.jsx
@@ -2,6 +2,11 @@ import React, {useState} from 'react';
 import './Pokeinfo.css';
 
 function Pokeinfo(){
+    const [buscaTermo, setBuscaTermo] = useState('');
+    const [pokemonDados, setPokemonDados] = useState(null);
+    const [erro, setErro] = useState('');
+
+    const Busca = async fetch('https://pokeapi.co/api/v2/pokemon')
     return(
         <>
             <h1>Informações sobre Pokemon</h1>
